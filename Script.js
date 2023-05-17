e61a7e624901b14f10b90e74c9d65ae6a83a9152
Nicolas Wangles <nicolaswangles@gmail.com>
2023-05-16 23:00:50 -0300

    Primeiro commit

---
+++ Script.js
@@ -0,0 +1,47 @@
+//Variaveis e seleção de elementos:
+const apiKey ="23fda6e778ddde79bc89cf428df674ec";
+
+const apiCountryURL = "https://flagsapi.com/.png";
+
+const cityInput = document.querySelector("#city-input");
+const searchBtn = document.querySelector("#search");
+
+//funções
+const showWeatherData = (city) => {
+    console.log();
+}
+
+//Eventos
+searchBtn.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+    
+    console.log("city");
+
+});
+
+
+function novaDiv() {
+    let btnDiv = document.createElement("div");
+    let botao = document.createElement("button");
+    let paragrafo = document.createElement("p");
+  
+    paragrafo.innerText = "Deseja salvar essa localização? ";
+  
+    btnDiv.id = "btnDiv";
+    btnDiv.className = "divDinamica";
+    botao.id = "botaoSalvar";
+    botao.innerText = "Salvar";
+  
+    btnDiv.appendChild(paragrafo);
+    btnDiv.appendChild(botao);
+  
+    document.body.append(btnDiv);
+    document.getElementById("search").disabled = true;
+  
+    botao.addEventListener("click", function () {
+      alert("Salvo!");
+      botao.disabled = true;
+    });
+  }
